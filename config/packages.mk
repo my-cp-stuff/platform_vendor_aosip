53fd41022fca8bfa2e189d2517768ca1e0c37e55
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    vendor: Port face unlock feature [2/5]
    Change-Id: I9d4bb7174d28ca067665893a8096b3176a5aa7a6

--- config/packages.mk
+++ config/packages.mk
@@ -61,3 +61,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     HideCutout \
     StatusBarStock
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
